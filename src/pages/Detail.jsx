ed8c187f721b2cbf53b8339352741fb67242b1e9
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2026-04-15 16:46:47 -0300

    Primeiros ajustes na estilização e adição de novos dados apresentados na página de detalhe

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -14,21 +14,52 @@ function Detail() {
      const getCountry = async () => {
           try {
                const response = await api.get(`/alpha/${code}`)
+               console.log(response.data[0])
                setCountry(response.data[0])
           } catch (error) {
                console.error('Error fetching country:', error)
           }
-          console.log(country)
      }
 
      if (!country) return <p>Loading...</p>
 
      return (
           <>
-               <div className='mainDetails'>
-                    <img src={country.flags.svg} alt={country.name.common} width={200} />
+               <div className='mainDetail'>
+                    <img src={country.flags.svg} width={200} />
                     <h1>{country.name.common}</h1>
                     <h2>{country.name.official}</h2>
+               </div>
+
+               <div className='otherDetails'>
+                    <div className='section'>
+                         <p className='name-info'>CAPITAL</p>
+                         <p className='info'>{country.capital[0]}</p>
+                         <p className='name-info'>CONTINENTE</p>
+                         <p className='info'>{country.continents[0]}</p>
+                         <p className='name-info'>SUB-REGIÃO</p>
+                         <p className='info'>{country.subregion}</p>
+                         <p className='name-info'>ÁREA</p>
+                         <p className='info'>{country.area} km²</p>
+                    </div >
+
+                    <div className='section'>
+                         <p className='name-info'>POPULAÇÃO</p>
+                         <p className='info'>{country.population}</p>
+
+                         <p className='name-info'>LÍNGUAS OFICIAIS</p>
+                         {Object.values(country.languages).map((language, index) => (
+                              <p key={index} className='info'>{language}</p>
+                         ))}
+
+                         <p className='name-info'>MOEDA</p>
+                         {Object.values(country.currencies).map((currency, index) => (
+                              <p key={index} className='info'>{currency.name}</p>
+                         ))}
+
+                         <p className='name-info'>CCA3</p>
+                         <p className='info'>{country.cca3}</p>
+                    </div >
                </div>
           </>
      )
