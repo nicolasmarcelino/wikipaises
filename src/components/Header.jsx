5b451a155984f15392edd81b9ba63221bcc77d45
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2026-04-17 12:57:04 -0300

    README atualizado e correção de caminho da imagem do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
      return (
           <div className="header">
-               <img src="src/assets/title.png" width="300"/>
+               <img src="/title.png" width="300"/>
           </div>
      )
 }
